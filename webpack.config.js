a6c98ddc0a5d1719f52bae2e7c212dd37f17be63
Dcterrance88 <>
2021-01-25 08:08:16 -0500

    Creando un archivo de estilos de forma global en la aplicación

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
     mode: 'development',
+    optimization:{
+        minimizer: [ new OptimizeCssAssetsPlugin()]
+    },
     module: {
         rules: [
+            {
+                test:/\.css$/,
+                exclude: /styles\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test:/styles\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
             {
                 test: /\.html$/i,
                 loader: 'html-loader',
@@ -19,5 +39,9 @@ module.exports = {
             template: './src/index.html',
             filenane: './index.html'
         }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            ignoreOrder: false
+        })
     ]
 }
